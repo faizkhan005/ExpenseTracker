300460a8b13ce705ca0ed95e7890dd260feebd29
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-11 08:52:53 -0400

    Added Missing Converter

---
+++ ExpenseTracker/Converters/NotifBgConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace ExpenseTracker.Converters;
+
+public class NotifBgConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is true  // IsRead = true
+            ? Color.FromArgb("#F5F5FA")
+            : Colors.White;
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
